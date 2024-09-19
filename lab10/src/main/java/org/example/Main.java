102eb0533f31cf6086fbbfc92f0958be82bd177e
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-09-19 13:09:46 +0300

    fix

--- lab10/src/main/java/org/example/Main.java
+++ lab10/src/main/java/org/example/Main.java
@@ -1,56 +1,80 @@
 package org.example;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
+import java.sql.*;
 
 public class Main {
+    static Connection connection;
+    static Statement statement;
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/task";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "8631";
-    private static Session session;
+    public static void main(String[] args) throws SQLException {
+        Connect();
 
-    public static void main(String[] args) {
-        session = HibernateUtil.getSessionFactory().openSession();
+        printStudentsPassedSubject();
+        printAverageGradeForSubject();
+        printAverageGradeForStudent();
+        printTop3Subjects();
 
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-             Scanner scanner = new Scanner(System.in)) {
+        Disconnect();
+    }
 
-            while (true) {
-                System.out.print("Введите скрипт: ");
-                String sql = scanner.nextLine();
+    public static void printStudentsPassedSubject() throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT s.name FROM students s JOIN progress p ON s.id = p.student_id WHERE p.subject_id = 7 AND p.grade > 3;");
+        System.out.println("Студенты, сдавшие предмет на оценку выше 3:");
+        while (rs.next()) {
+            System.out.println("- " + rs.getString(1));  // Выводим имя студента
+        }
+        System.out.println();
+    }
 
-                // Выполнение SQL-скрипта
-                try (Statement statement = connection.createStatement()) {
-                    boolean hasResultSet = statement.execute(sql);
+    public static void printAverageGradeForSubject() throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT round(AVG(grade), 1) AS average_grade FROM progress WHERE subject_id = 5;");
+        if (rs.next()) {
+            System.out.println("Средний балл по предмету: " + rs.getDouble(1));
+        }
+        System.out.println();
+    }
 
-                    if (hasResultSet) {
+    public static void printAverageGradeForStudent() throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT round(AVG(grade), 1) AS average_grade FROM progress WHERE student_id = 7;");
+        if (rs.next()) {
+            System.out.println("Средний балл студента: " + rs.getDouble(1));
+        }
+        System.out.println();
+    }
 
-                        ResultSet resultSet = statement.getResultSet();
-                        int columnCount = resultSet.getMetaData().getColumnCount();
+    public static void printTop3Subjects() throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT s.subject_name, COUNT(p.student_id) AS student_count FROM subjects s JOIN progress p ON s.id = p.subject_id GROUP BY s.subject_name ORDER BY student_count DESC LIMIT 3;");
+        System.out.println("Три предмета, которые сдали наибольшее количество студентов:");
+        while (rs.next()) {
+            System.out.println("- Предмет: " + rs.getString(1) + ", Количество студентов: " + rs.getInt(2));
+        }
+        System.out.println();
+    }
 
-                        // Выводим результаты
-                        while (resultSet.next()) {
-                            for (int i = 1; i <= columnCount; i++) {
-                                System.out.print(resultSet.getString(i) + "\t");
-                            }
-                            System.out.println();
-                        }
-                    }
-                } catch (SQLException e) {
-                    System.err.println("Ошибка при выполнении запроса: " + e.getMessage());
-                }
-            }
+    // Подключение к БД
+    public static void Connect() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/task",
+                    "postgres",
+                    "8631"
+            );
+            statement = connection.createStatement();
         } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        }
+    }
+
+    // Отключение от БД
+    public static void Disconnect() throws SQLException {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            statement.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
